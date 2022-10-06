ec6e0bcd73b1c3090671e31257e3f8c8662d1ebe
Arzz <113747900+Arzzzzy@users.noreply.github.com>
2022-10-06 21:22:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-#def to_share(total_candies, n_friends=4):
-    #return total_candies % n_friends
+#@arzadon
+#CASE 1:
+def match(x, y):
+    val = min(abs(x), abs(y))
+    print(val)
 
-#print(to_share(91))
-#print(to_share(91, 3))
+(match(-10, 5))
+(match(0, -2))
+(match(10, -21))
+(match(-5, 25))
+
+#CASE 2:
+def smallest_abs(x, y):
+    val = min(abs(x), abs(y))
+     return val
+
+ print(smallest_abs(-10, 5))
+ print(smallest_abs(0, -2))
+ print(smallest_abs(10, -21))
+ print(smallest_abs(-5, 25))
+    
 
 
-def to_share(total_candies, n_friends=3):
-    return total_candies % n_friends
 
-print(to_share(91))
-print(to_share(10, 4))
-print(to_share(5))
-print(to_share(1, 5))
 
 
 
